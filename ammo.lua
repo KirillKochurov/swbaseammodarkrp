11e1bc72f65ab426037869a41db1ef529715573a
kirill123550 <61646821+kirill123550@users.noreply.github.com>
2021-01-11 21:35:26 +0300

    Create ammo.lua

---
+++ ammo.lua
@@ -0,0 +1,31 @@
+DarkRP.createAmmoType("Rifle", {
+	name = "SWBase Rifle",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 40,
+	amountGiven = 60
+})
+DarkRP.createAmmoType("Buckshot", {
+	name = "SWBase Buckshot",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 40,
+	amountGiven = 60
+})
+DarkRP.createAmmoType("Pistol", {
+	name = "SWBase Pistol",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 40,
+	amountGiven = 60
+})
+--smg1
+DarkRP.createAmmoType("smg1", {
+	name = "SWBase SMG",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 40,
+	amountGiven = 60
+})
+DarkRP.createAmmoType("Rifle", {
+	name = "SWBase Sniper",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 40,
+	amountGiven = 60
+})
